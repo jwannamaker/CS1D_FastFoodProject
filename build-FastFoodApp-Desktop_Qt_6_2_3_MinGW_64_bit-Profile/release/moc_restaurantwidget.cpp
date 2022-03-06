e47c54a5cbf850f7f39d590b102c7abdcc152c9c
dantevazquez <dantesurf9@gmail.com>
2022-03-06 10:56:56 -0800

    build changes

---
+++ build-FastFoodApp-Desktop_Qt_6_2_3_MinGW_64_bit-Profile/release/moc_restaurantwidget.cpp
@@ -0,0 +1,156 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'restaurantwidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../FastFoodApp/restaurantwidget.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'restaurantwidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_RestaurantWidget_t {
+    const uint offsetsAndSize[12];
+    char stringdata0[101];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_RestaurantWidget_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_RestaurantWidget_t qt_meta_stringdata_RestaurantWidget = {
+    {
+QT_MOC_LITERAL(0, 16), // "RestaurantWidget"
+QT_MOC_LITERAL(17, 15), // "transmit_cancel"
+QT_MOC_LITERAL(33, 0), // ""
+QT_MOC_LITERAL(34, 24), // "on_confirmButton_pressed"
+QT_MOC_LITERAL(59, 23), // "on_cancelButton_pressed"
+QT_MOC_LITERAL(83, 17) // "restaurantClicked"
+
+    },
+    "RestaurantWidget\0transmit_cancel\0\0"
+    "on_confirmButton_pressed\0"
+    "on_cancelButton_pressed\0restaurantClicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_RestaurantWidget[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   38,    2, 0x06,    1 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       3,    0,   39,    2, 0x08,    2 /* Private */,
+       4,    0,   40,    2, 0x08,    3 /* Private */,
+       5,    0,   41,    2, 0x08,    4 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void RestaurantWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<RestaurantWidget *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->transmit_cancel(); break;
+        case 1: _t->on_confirmButton_pressed(); break;
+        case 2: _t->on_cancelButton_pressed(); break;
+        case 3: _t->restaurantClicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (RestaurantWidget::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&RestaurantWidget::transmit_cancel)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject RestaurantWidget::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_RestaurantWidget.offsetsAndSize,
+    qt_meta_data_RestaurantWidget,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_RestaurantWidget_t
+, QtPrivate::TypeAndForceComplete<RestaurantWidget, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *RestaurantWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *RestaurantWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_RestaurantWidget.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int RestaurantWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void RestaurantWidget::transmit_cancel()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
